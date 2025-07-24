d605204efb7b78c47fd295947890c3e11cf5d57d
Kevin Su <kevinsu2024@u.northwestern.edu>
2025-07-24 18:58:34 -0400

    update paths to start from the api/ root.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 
 import express, { Express } from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use('/api', routes);
 
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:3000`);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
